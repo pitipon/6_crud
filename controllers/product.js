b4a1b7c408730f0264efbba81bf2302c70b56a5d
Pitipon Pluemworasawat <pitipon@gmail.com>
2021-09-26 01:14:59 +0800

    update

--- controllers/product.js
+++ controllers/product.js
@@ -1,3 +1,5 @@
+import Product from "../models/product";
+
 
 
 // Create and Save a new product
@@ -6,25 +8,30 @@ export const create = (req, res) => {
   const body_name = req.body.name
   const body_price = req.body.prices
 
-  res.json({
-    name: body_name,
-    price: body_price,
-    _id: '3434234233434'
-  });
+  try {
+    // create product
+    const product = await new Product({
+      name: body_name,
+      price: body_price,
+    }).save();
+
+    res.json(product);
+  } catch (err) {
+    // error
+    console.log(err);
+    return res.status(400).send("Course create failed. Try again.");
+  }
 };
+
+
 
 // Retrieve all products from the database.
 export const findAll = (req, res) => {
-  res.json([{
-    name: 'milk',
-    price: 20,
-    _id: '3434234233434'
-  },{
-    name: 'coke',
-    price: 25,
-    _id: '7746462232232'
-  }]);
+  const all = await Product.find()
+  res.json(all);
 };
+
+
 
 // Find a single product with an id
 export const findOne = (req, res) => {
@@ -32,36 +39,52 @@ export const findOne = (req, res) => {
   // Find product by id
   const id = req.params.id
 
-  res.json({
-    name: 'milk',
-    price: 20,
-    _id: id
-  });
+  try {
+    // find product by id
+    const product = await Product.findOne({ _id: id }).exec();
+    res.json(product);
+  } catch (err) {
+    // error
+    console.log(err);
+    return res.status(400).send("Course create failed. Try again.");
+  }
 };
+
+
 
 // Update a product by the id in the request
 export const updateOne = (req, res) => {
   // Get id for update product
   const id = req.params.id
 
-  // Get body
-  const body_name = req.params.name
-  const body_price = req.params.price
+  try {
+    const updated = await Product.findOneAndUpdate({ _id: id}, req.body, {
+      new: true,
+    }).exec();
 
-  res.json({
-    name: body_name,
-    price: body_price,
-    _id: id
-  });
+    res.json(updated);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err.message);
+  }
 };
+
+
+
 
 // Delete a product with the specified id in the request
 export const deleteOne = (req, res) => {
   // Get id for delete
   const id = req.params.id
 
-  res.json({
-    status: 'deleted',
-    _id: id
-  });
+  try {
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) response.status(404).send("No item found");
+    response.status(200).send();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send(err.message);
+  }
 };
